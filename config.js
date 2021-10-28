ccc2b1497ca24cc15dc29b7f964750ea7fef27d4
Baks <baks.odessa@gmail.com>
2021-10-28 13:03:41 +0300

    add cli for extract args end env

--- config.js
+++ config.js
@@ -1,6 +1,10 @@
+const cli = require('./lib/cli');
+
+const app_port = cli.args.APP_PORT ? parseInt(cli.args.APP_PORT, 10) : 3000;
+
 const config = {
-  app_port: process.env.APP_PORT || 3000,
-  env: process.env,
+  app_port,
+  env: cli.env,
 };
 
 module.exports = config;
